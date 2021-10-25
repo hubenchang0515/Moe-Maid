e20c545578b6855ea4b0d7a803a73eb5bce957e3
PlanC <hubenchang0515@outlook.com>
2021-10-25 13:41:21 +0800

    git diff commit

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Moe-Maid
-萌女仆 —— 方便日常生活的脚本工具仓库
+萌女仆 —— 方便日常生活的脚本工具仓库  
 
-* [charimg.py](charimg.py) : 根据图片生成字符画
-* [today.py](today.py) : 查看日历
-* [mark.py](mark.py) : 根据Markdown生成HTML
+* [charimg.py](charimg.py) : 根据图片生成字符画  
+* [today.py](today.py) : 查看日历  
+* [mark.py](mark.py) : 根据Markdown生成HTML  
 * [gmark.py](gmark.py) : 根据Markdown生成HTML，通过GitHub的API  
-* [docker-tags.py](docker-tags.py) : 搜索Docker镜像标签
-* [spending.py](spending.py) : Linux进程开销检测
+* [docker-tags.py](docker-tags.py) : 搜索Docker镜像标签  
+* [spending.py](spending.py) : Linux进程开销检测  
+* [git-diff-commit.sh](git-diff-commit.sh) : 比较git上两个分支点commit差异  
---
+++ git-diff-commit.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/bash
+
+# 此脚本用于git分支合并时,比较两个分支之间的提交差异
+# git cherry-pick 会生成新的提交和哈希值
+# 导致无法比较两个分支之间不同的commit时会包含通过cherry-pick摘取过的提交
+# 此脚本根据提交的标题来比较提交是否相同,打印两个分支之间的不同提交
+
+if [ "$#" != "2" ]
+then
+    echo "Usage: $0 <target-branch> <source-branch>" >> /dev/stderr
+    exit 1
+fi
+
+TARGET_BRANCH=$1
+SOURCE_BRANCH=$2
+
+COMMIT_LIST=$(git log --pretty=format:"%H" ^$TARGET_BRANCH $SOURCE_BRANCH)
+
+declare -a DIFF_COMMIT_LIST
+
+for commit in ${COMMIT_LIST[@]}
+do
+    title=$(git log --pretty=format:"%s" -1 $commit)
+    git log --pretty=format:"%s" $TARGET_BRANCH | grep "$title" >> /dev/null
+    if [ "$?" != "0" ]
+    then
+        DIFF_COMMIT_LIST[${#DIFF_COMMIT_LIST[@]}]=$commit
+    else
+        echo "Skip same title: $title" >> /dev/stderr
+    fi
+done
+
+for commit in ${DIFF_COMMIT_LIST[@]}
+do
+    echo $commit
+done
